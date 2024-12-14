c3416bd2f2fe9f6a527c482ddb5d47c01fec191d
Justin Wang <85368198+jstwng@users.noreply.github.com>
2024-12-14 00:19:30 -0800

    header & about

---
+++ site/src/Bar.tsx
@@ -0,0 +1,12 @@
+function Bar() {
+  return (
+    <>
+      <h1 className="bar-title">About</h1>
+      <h1 className="bar-title">The Catalyst Program</h1>
+      <h1 className="bar-title">FAQs</h1>
+      <h1 className="bar-title">Sign In</h1>
+    </>
+  );
+}
+
+export default Bar;
---
+++ site/src/Landing.tsx
@@ -0,0 +1,12 @@
+function Landing() {
+  return (
+    <>
+      <h1 style={{ fontSize: "55px" }}>
+        Join Atlanta's best and brightest students in solving real-world
+        economic problems.{" "}
+      </h1>
+    </>
+  );
+}
+
+export default Landing;
