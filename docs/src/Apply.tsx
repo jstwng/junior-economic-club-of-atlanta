8b98e18f0a593dcae000b96f0a3e75bd780fcdce
Justin Wang <85368198+jstwng@users.noreply.github.com>
2025-01-16 23:12:14 -0800

    fix stale minified css/js

--- docs/src/Apply.tsx
+++ docs/src/Apply.tsx
@@ -24,9 +24,9 @@ createRoot(document.getElementById("root")!).render(
         src="https://docs.google.com/forms/d/e/1FAIpQLScPoGnBGtHOvwhHa6uIn8ybPh_-U0Iai8D7adgFGDJR2PYVCQ/viewform?embedded=true"
         width="100%"
         height="800px"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
       >
         Loading…
       </iframe>
